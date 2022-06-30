dcf1918fb835e62a6f165587c0b6647ab0a8a429
Angel Concepcion <angelconcepcion50@gmail.com>
2022-06-30 17:16:35 -0400

    working solution

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
+
     public static void main(String[] args) {
-        // your code here
+        String gradingMethod = "SLIGHT"; // <---- simply change this value to change the way grades are curved
+        StudentGradeTranslator gradeTranslator = new StudentGradeTranslator();
+
+        HashMap<String, String> studentGrades = new HashMap<String, String>();
+        studentGrades.put("Jamaal", "93");
+        studentGrades.put("Jennifer", "87");
+        studentGrades.put("Jules", "59");
+
+        // get all the student and their grades using each entry
+        System.out.println("List of students and their grades:");
+        for (Map.Entry<String, String> studentGrade: studentGrades.entrySet()) {
+            System.out.println(studentGrade.getKey() + "'s grade is " +
+                    gradeTranslator.getLetterGrade(Integer.parseInt(studentGrade.getValue())));
+            System.out.println("Passing grade status: " + gradeTranslator.isPassingGrade(Integer.parseInt(studentGrade.getValue())));
+        }
     }
 }
